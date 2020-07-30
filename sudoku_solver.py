1be1faf028f335eea4039af0dd41e0ed329eed84
Ben Roberts <benjamindaveroberts@gmail.com>
2020-07-30 18:30:42 +0100

    strengthens contains_invalid_values method
    Also added clarifications to docs

--- sudoku_solver.py
+++ sudoku_solver.py
@@ -81,9 +81,10 @@ class SudokuPuzzle:
 
 	def is_valid(self):
 		"""
-		Checks that no value is repeated in a row, column or subgrid (i.e. the 3x3 tiles in a 9x9 grid).
+		Checks that no value is repeated in a row, column or subgrid (i.e. the 3x3 tiles in a 9x9 grid)
+		and checks that all values are members of the number set or 0.
 
-		:return: True if every value in evey row/column is unique in that row. False otherwise
+		:return: True if every value in evey row/column is unique in that row. False otherwise.
 		"""
 		valid = not self.contains_invalid_values()
 		for i in range(self.side_length):
@@ -92,24 +93,34 @@ class SudokuPuzzle:
 			# * (puzzle is wrong if number set is longer)
 			valid = valid and len(set(self.get_row(i))) == self.side_length
 			valid = valid and len(set(self.get_column(i))) == self.side_length
+			valid = valid and len(set(
+				[x for x in y
+				 for y in self.get_subgrid(r, c)
+				 for r in range(0, self.side_length, self.sub_side_length)
+				 for c in range(0, self.side_length, self.sub_side_length)])) == self.side_length
 
 		return valid
 
 	def is_complete(self):
+		"""
+		Checks that all the values in the grid are members of the number set
+		and that all values in a row/column/subgrid are unique to each other.
+		The combination of these checks is sufficient to say whether the puzzle is complete.
+
+		:return: True if the puzzle is complete. False otherwise.
+		"""
 		return self.is_full() and self.is_valid()
 
 	def contains_invalid_values(self):
 		"""
-		Checks that the puzzle doesn't contain values that is shouldn't.
-		These values are anything not in the puzzles number set or 0.
+		Checks that the puzzle doesn't contain values that are not members of its number set
+		or the empty placeholder value 0.
 
 		:return: True if the puzzle contains values it shouldn't. False otherwise.
 		"""
 		invalid = False
 		for i in range(self.side_length):
-			invalid = invalid or any([x not in self.number_set and x != 0 for x in self.get_row(i)])
-			# Could remove the above or below line (but not both) without affecting this method
-			invalid = invalid or any([x not in self.number_set and x != 0 for x in self.get_column(i)])
+			invalid = invalid or any([x not in set.union(self.number_set, {0}) for x in self.get_row(i)])
 		return invalid
 
 	def __generate_puzzle(self):
@@ -131,11 +142,6 @@ class SudokuPuzzle:
 
 	def solve(self):
 		change_made = False
-
-		def check_complete():
-			if self.is_complete():
-				print(f"PUZZLE SOLVED")
-				return True
 
 		def fill_known_subgrid_values():
 			"""
@@ -251,16 +257,16 @@ class SudokuPuzzle:
 			raise Exception("The puzzle that is trying to be solved is invalid and will not have a solution.")
 
 		fill_known_subgrid_values()
-		if check_complete():
-			print("SOLVED")
+		if self.is_complete():
+			# print("SOLVED")
 			return None
 		fill_singleton_possibilities()
-		if check_complete():
-			print("SOLVED")
+		if self.is_complete():
+			# print("SOLVED")
 			return None
 		fill_known_row_column_values()
-		if check_complete():
-			print("SOLVED")
+		if self.is_complete():
+			# print("SOLVED")
 			return None
 
 		if not change_made:
